26bfb8a510a0cf3fcc34d802c512cac594df5197
gunaevart <gunaevart@mail.ru>
2018-10-03 15:27:42 +0300

    добавленно модальное окно в шапку сайта

--- app/js/common.js
+++ app/js/common.js
@@ -2,16 +2,39 @@ $(function() {
 
 	// заказать звонок
 
-	$('#orderPhone').click(function(){
-		console.log('заказать звонок')
-	})
+	$('#getFormOrder').click(function(){
+		// получаем данные из формы
+		let fioModal = $('#fioModal').val();
+		let mailModal = $('#mailModal').val();
+		let phoneModal = $('#phoneModal').val();
+		// текущая дата
+		// var d = new Date();
+		// var curr_date = d.getDate();
+		// var curr_month = d.getMonth() + 1;
+		// var curr_year = d.getFullYear();
+
+
+		console.log('Имя: '+ fioModal + 'Почта: ' + mailModal + 'Телефон: ' + phoneModal);
+
+
+		// Закрывам модалку
+		$('#modalHeader, .modal-backdrop').fadeOut(500);
+		$('#modalHeaderStatus').show(1000);
+
+		// Открываеться модалка успешной заявки
+		$('#close, .close').click(function(){
+			$('#modalHeaderStatus').hide(500);
+		});		
+
+		
+	});
 
 
 	// заказать товар
 
 	$('#btnPhone').click(function(){
-		console.log('заказать товар')
-	})
+		console.log('заказать товар');
+	});
 	// $('#inputPhone').
 
 });
