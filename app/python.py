e24261386fff8dddcf2164e5f6daf503ee24d5e4
gunaevart <gunaevart@mail.ru>
2018-10-04 15:41:15 +0300

    Мобильная адаптация

--- app/python.py
+++ app/python.py
@@ -1,6 +1,24 @@
-print(123)
-
-age = 33
 
 
-print('привет мне ' + str(age))
+
+obj = {
+    'name':'victot',
+    'age':'21',
+    'pol':'m',
+    'phine':789987,
+}
+
+all_users = []
+for i in range(0, 3):
+    all_users.append(obj.copy())
+for i in all_users:
+    print(i)
+
+all_users[1]['name'] = 'vova'
+all_users[1]['age'] = 33
+
+
+print('____________________')
+
+for i in all_users:
+    print(i)
